fd43be3f7e481bda3c5823d553e3e77617611fc0
Adam Blake <theadamattack@gmail.com>
2018-12-12 17:47:52 -0800

    Allow pass-through to lm from extraction functions

--- supernova/R/estimate_extraction.R
+++ supernova/R/estimate_extraction.R
@@ -5,7 +5,7 @@
 #' @param fit A \code{\link{lm}} object.
 #' @param formula A \code{\link{formula}}.
 #' @param data A \code{\link{data.frame}}.
-#' @param ... Passthrough arguments.
+#' @param ... Passthrough arguments to \code{\link{lm}}.
 #'
 #' @return The PRE value of the relevant model.
 #'
@@ -20,7 +20,7 @@ PRE <- function(fit, ...) {
 
 #' @rdname PRE
 #' @export
-PRE.default <- function(fit, ...) {
+PRE.default <- function(fit) {
   pre <- summary(fit)$r.squared[1]
   names(pre) <- NULL
   pre
@@ -29,7 +29,7 @@ PRE.default <- function(fit, ...) {
 #' @rdname PRE
 #' @export
 PRE.formula <- function(formula, data = list(), ...) {
-  fit <- lm(formula, data)
+  fit <- lm(formula, data, ...)
   pre <- PRE.default(fit)
   pre
 }
@@ -41,7 +41,7 @@ PRE.formula <- function(formula, data = list(), ...) {
 #' @param fit A \code{\link{lm}} object.
 #' @param formula A \code{\link{formula}}.
 #' @param data A \code{\link{data.frame}}.
-#' @param ... Passthrough arguments.
+#' @param ... Passthrough arguments to \code{\link{lm}}.
 #'
 #' @return The F value of the relevant model.
 #'
@@ -56,7 +56,7 @@ fVal <- function(fit, ...) {
 
 #' @rdname fVal
 #' @export
-fVal.default <- function(fit, ...) {
+fVal.default <- function(fit) {
   fval <- summary(fit)$fstatistic[1]
   names(fval) <- NULL
   fval
@@ -65,7 +65,7 @@ fVal.default <- function(fit, ...) {
 #' @rdname fVal
 #' @export
 fVal.formula <- function(formula, data = list(), ...) {
-  fit <- lm(formula, data)
+  fit <- lm(formula, data, ...)
   fval <- fVal.default(fit)
   fval
 }
@@ -77,7 +77,7 @@ fVal.formula <- function(formula, data = list(), ...) {
 #' @param fit A \code{\link{lm}} object.
 #' @param formula A \code{\link{formula}}.
 #' @param data A \code{\link{data.frame}}.
-#' @param ... Passthrough arguments.
+#' @param ... Passthrough arguments to \code{\link{lm}}.
 #'
 #' @return The intercept of the relevant model.
 #'
@@ -92,7 +92,7 @@ b0 <- function(fit, ...) {
 
 #' @rdname b0
 #' @export
-b0.default <- function(fit, ...) {
+b0.default <- function(fit) {
   bz <- fit$coefficients[1]
   names(bz) <- NULL
   bz
@@ -101,7 +101,7 @@ b0.default <- function(fit, ...) {
 #' @rdname b0
 #' @export
 b0.formula <- function(formula, data = list(), ...) {
-  fit <- lm(formula, data)
+  fit <- lm(formula, data, ...)
   bz <- b0.default(fit)
   bz
 }
@@ -113,7 +113,7 @@ b0.formula <- function(formula, data = list(), ...) {
 #' @param fit A \code{\link{lm}} object.
 #' @param formula A \code{\link{formula}}.
 #' @param data A \code{\link{data.frame}}.
-#' @param ... Passthrough arguments.
+#' @param ... Passthrough arguments to \code{\link{lm}}.
 #'
 #' @return The slope of the relevant model.
 #'
@@ -128,7 +128,7 @@ b1 <- function(fit, ...) {
 
 #' @rdname b1
 #' @export
-b1.default <- function(fit, ...) {
+b1.default <- function(fit) {
   bo <- fit$coefficients[2]
   names(bo) <- NULL
   bo
@@ -137,7 +137,7 @@ b1.default <- function(fit, ...) {
 #' @rdname b1
 #' @export
 b1.formula <- function(formula, data = list(), ...) {
-  fit <- lm(formula, data)
+  fit <- lm(formula, data, ...)
   bo <- b1.default(fit)
   bo
 }
