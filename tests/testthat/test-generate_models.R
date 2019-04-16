072243322a93e4d0d36bb55d7807527db807aa3d
Adam Blake <theadamattack@gmail.com>
2019-04-16 13:41:17 -0700

    Change generate_models structure and add readable printout
    * Re-arrange structure of generate_models so that each key of the output
      corresponds to a term in the model, and the `complex` and `simple` model at
      that key can be compared to get the appropriate SS for the given type.
    
    * Change print output to only show the formula for each comparison model.
    
    * Add additional tests to `generate_models()` for `lm()` objects to check that
      their output is consistent with output when using `formula()` for models.
    
    * Add tests and code for empty, one-way, and two-way models

--- tests/testthat/test-generate_models.R
+++ tests/testthat/test-generate_models.R
@@ -2,105 +2,322 @@ context("Generate models for different SS types")
 
 library(dplyr)
 
-test_formula <- y ~ a + b + c * a:b + a:c + b:c + a:b:c
+test_formula <- y ~ a * b * c
+test_terms <- c("Full Model", "a", "b", "c", "a:b", "a:c", "b:c", "a:b:c")
+
+expect_attr <- function(obj, attribute, expected) {
+  expect_equal(attr(obj, attribute), expected)
+}
+
+test_that("model has attribute for type specification", {
+  expect_attr(generate_models(test_formula, 1), "type", "I")
+  expect_attr(generate_models(test_formula, 2), "type", "II")
+  suppressWarnings(
+    expect_attr(generate_models(test_formula, 3), "type", "III")
+  )
+})
+
+test_that("model has attribute for model specification", {
+  expect_attr(generate_models(test_formula, 1), "model", y ~ a * b * c)
+})
 
 test_that("generating Type 3 models emits a message regarding `anova()`", {
   expect_warning(generate_models(test_formula, 3))
 })
 
 test_that("generated models are organized with the key as the term", {
-  expected <- c("a", "b", "c", "a:b", "a:c", "b:c", "a:b:c")
-  expect_identical(names(type_1_models(test_formula)), expected)
-  expect_identical(names(type_2_models(test_formula)), expected)
-  expect_identical(names(type_3_models(test_formula)), expected)
+  generate_models(test_formula, 1) %>%
+    expect_length(length(test_terms)) %>%
+    expect_named(test_terms)
+})
+
+test_that("each term has a key for each of its complex and simple models", {
+  generate_models(test_formula, 1) %>%
+    purrr::flatten() %>%
+    expect_named(rep(c("complex", "simple"), times = length(test_terms)))
 })
 
 test_that("sequential models are returned for Type 1", {
   expected <- list(
-    augmented = list(
-      `a`     = y ~                               a,
-      `b`     = y ~ a +                           b,
-      `c`     = y ~ a + b +                       c,
-      `a:b`   = y ~ a + b + c +                   a:b,
-      `a:c`   = y ~ a + b + c + a:b +             a:c,
-      `b:c`   = y ~ a + b + c + a:b + a:c +       b:c,
-      `a:b:c` = y ~ a + b + c + a:b + a:c + b:c + a:b:c
+    `Full Model` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c + a:b:c,
+      simple = y ~ NULL
     ),
-    compact = list(
-      `a`     = y ~ NULL,
-      `b`     = y ~ a,
-      `c`     = y ~ a + b,
-      `a:b`   = y ~ a + b + c,
-      `a:c`   = y ~ a + b + c + a:b,
-      `b:c`   = y ~ a + b + c + a:b + a:c,
-      `a:b:c` = y ~ a + b + c + a:b + a:c + b:c
+    `a` = list(
+      complex = y ~ a,
+      simple  = y ~ NULL
+    ),
+    `b` = list(
+      complex = y ~ a + b,
+      simple  = y ~ a
+    ),
+    `c` = list(
+      complex = y ~ a + b + c,
+      simple  = y ~ a + b
+    ),
+    `a:b` = list(
+      complex = y ~ a + b + c + a:b,
+      simple  = y ~ a + b + c
+    ),
+    `a:c` = list(
+      complex = y ~ a + b + c + a:b + a:c,
+      simple  = y ~ a + b + c + a:b
+    ),
+    `b:c` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c,
+      simple  = y ~ a + b + c + a:b + a:c
+    ),
+    `a:b:c` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c + a:b:c,
+      simple  = y ~ a + b + c + a:b + a:c + b:c
     )
   )
-  expect_equal(generate_models(test_formula, type = 1), expected)
+  expect_equivalent(generate_models(test_formula, type = 1), expected)
 })
 
 test_that("hierarchical models are returned for Type 2", {
   expected <- list(
-    augmented = list(
-      `a`     = y ~     b + c +             b:c + a,
-      `b`     = y ~ a +     c +       a:c +       b,
-      `c`     = y ~ a + b +     a:b +             c,
-      `a:b`   = y ~ a + b + c +       a:c + b:c + a:b,
-      `a:c`   = y ~ a + b + c + a:b +       b:c + a:c,
-      `b:c`   = y ~ a + b + c + a:b + a:c +       b:c,
-      `a:b:c` = y ~ a + b + c + a:b + a:c + b:c + a:b:c
+    `Full Model` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c + a:b:c,
+      simple = y ~ NULL
     ),
-    compact = list(
-      `a`     = y ~     b + c +             b:c,
-      `b`     = y ~ a +     c +       a:c,
-      `c`     = y ~ a + b +     a:b,
-      `a:b`   = y ~ a + b + c +       a:c + b:c,
-      `a:c`   = y ~ a + b + c + a:b +       b:c,
-      `b:c`   = y ~ a + b + c + a:b + a:c,
-      `a:b:c` = y ~ a + b + c + a:b + a:c + b:c
+    `a` = list(
+      complex = y ~ a + b + c             + b:c,
+      simple  = y ~     b + c             + b:c
+    ),
+    `b` = list(
+      complex = y ~ a + b + c       + a:c,
+      simple  = y ~ a     + c       + a:c
+    ),
+    `c` = list(
+      complex = y ~ a + b + c + a:b,
+      simple  = y ~ a + b     + a:b
+    ),
+    `a:b` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c,
+      simple  = y ~ a + b + c       + a:c + b:c
+    ),
+    `a:c` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c,
+      simple  = y ~ a + b + c + a:b       + b:c
+    ),
+    `b:c` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c,
+      simple  = y ~ a + b + c + a:b + a:c
+    ),
+    `a:b:c` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c + a:b:c,
+      simple  = y ~ a + b + c + a:b + a:c + b:c
     )
   )
-  expect_equal(generate_models(test_formula, type = 2), expected)
+  expect_equivalent(generate_models(test_formula, type = 2), expected)
 })
 
 test_that("orthogonal models are returned for Type 3", {
   expected <- list(
-    augmented = list(
-      `a`     = y ~     b + c + a:b + a:c + b:c + a:b:c + a,
-      `b`     = y ~ a +     c + a:b + a:c + b:c + a:b:c + b,
-      `c`     = y ~ a + b +     a:b + a:c + b:c + a:b:c + c,
-      `a:b`   = y ~ a + b + c +       a:c + b:c + a:b:c + a:b,
-      `a:c`   = y ~ a + b + c + a:b +       b:c + a:b:c + a:c,
-      `b:c`   = y ~ a + b + c + a:b + a:c +       a:b:c + b:c,
-      `a:b:c` = y ~ a + b + c + a:b + a:c + b:c +         a:b:c
+    `Full Model` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c + a:b:c,
+      simple = y ~ NULL
     ),
-    compact = list(
-      `a`     = y ~     b + c + a:b + a:c + b:c + a:b:c,
-      `b`     = y ~ a +     c + a:b + a:c + b:c + a:b:c,
-      `c`     = y ~ a + b +     a:b + a:c + b:c + a:b:c,
-      `a:b`   = y ~ a + b + c +       a:c + b:c + a:b:c,
-      `a:c`   = y ~ a + b + c + a:b +       b:c + a:b:c,
-      `b:c`   = y ~ a + b + c + a:b + a:c +       a:b:c,
-      `a:b:c` = y ~ a + b + c + a:b + a:c + b:c
+    `a` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c + a:b:c,
+      simple  = y ~     b + c + a:b + a:c + b:c + a:b:c
+    ),
+    `b` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c + a:b:c,
+      simple  = y ~ a     + c + a:b + a:c + b:c + a:b:c
+    ),
+    `c` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c + a:b:c,
+      simple  = y ~ a + b     + a:b + a:c + b:c + a:b:c
+    ),
+    `a:b` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c + a:b:c,
+      simple  = y ~ a + b + c       + a:c + b:c + a:b:c
+    ),
+    `a:c` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c + a:b:c,
+      simple  = y ~ a + b + c + a:b       + b:c + a:b:c
+    ),
+    `b:c` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c + a:b:c,
+      simple  = y ~ a + b + c + a:b + a:c       + a:b:c
+    ),
+    `a:b:c` = list(
+      complex = y ~ a + b + c + a:b + a:c + b:c + a:b:c,
+      simple  = y ~ a + b + c + a:b + a:c + b:c
     )
   )
   suppressWarnings(generate_models(test_formula, type = 3)) %>%
-    expect_equal(expected)
+    expect_equivalent(expected)
 })
 
 test_that("model class is preserved", {
   model <- lm(mpg ~ hp * disp, data = mtcars)
   expected <- list(
-    augmented = list(
-      `hp`      = supernova:::update(model, mpg ~ hp),
-      `disp`    = supernova:::update(model, mpg ~ hp + disp),
-      `hp:disp` = supernova:::update(model, mpg ~ hp + disp + hp:disp)
+    `Full Model` = list(
+      complex = supernova:::update(model, mpg ~ hp + disp + hp:disp),
+      simple  = supernova:::update(model, mpg ~ NULL)
     ),
-    compact = list(
-      `hp`      = supernova:::update(model, mpg ~ NULL),
-      `disp`    = supernova:::update(model, mpg ~ hp),
-      `hp:disp` = supernova:::update(model, mpg ~ hp + disp)
+    `hp` = list(
+      complex = supernova:::update(model, mpg ~ hp),
+      simple  = supernova:::update(model, mpg ~ NULL)
+    ),
+    `disp` = list(
+      complex = supernova:::update(model, mpg ~ hp + disp),
+      simple  = supernova:::update(model, mpg ~ hp)
+    ),
+    `hp:disp` = list(
+      complex = supernova:::update(model, mpg ~ hp + disp + hp:disp),
+      simple  = supernova:::update(model, mpg ~ hp + disp)
     )
   )
-  expect_equal(generate_models(model, type = 1), expected)
+  expect_equivalent(generate_models(model, type = 1), expected)
+})
+
+test_that("two-way models work", {
+  model <- lm(mpg ~ hp * disp, data = mtcars)
+  expected <- list(
+    `Full Model` = list(
+      complex = supernova:::update(model, mpg ~ hp + disp + hp:disp),
+      simple  = supernova:::update(model, mpg ~ NULL)
+    ),
+    `hp` = list(
+      complex = supernova:::update(model, mpg ~ hp),
+      simple  = supernova:::update(model, mpg ~ NULL)
+    ),
+    `disp` = list(
+      complex = supernova:::update(model, mpg ~ hp + disp),
+      simple  = supernova:::update(model, mpg ~ hp)
+    ),
+    `hp:disp` = list(
+      complex = supernova:::update(model, mpg ~ hp + disp + hp:disp),
+      simple  = supernova:::update(model, mpg ~ hp + disp)
+    )
+  )
+  expect_equivalent(generate_models(model, type = 1), expected)
+})
+
+test_that("one-way models work", {
+  model <- lm(mpg ~ hp, data = mtcars)
+  expected <- list(
+    `Full Model` = list(
+      complex = supernova:::update(model, mpg ~ hp),
+      simple  = supernova:::update(model, mpg ~ NULL)
+    ),
+    `hp` = list(
+      complex = supernova:::update(model, mpg ~ hp),
+      simple  = supernova:::update(model, mpg ~ NULL)
+    )
+  )
+  expect_equivalent(generate_models(model, type = 1), expected)
+})
+
+test_that("null models return NULL", {
+  model <- lm(mpg ~ NULL, data = mtcars)
+  expect_equivalent(generate_models(model, type = 1), list())
+})
+
+
+# Printing ----------------------------------------------------------------
+
+test_that("model object has explanatory header", {
+  printed <- capture.output(generate_models(test_formula, type = 1))
+  expect_identical(printed[1:3], c(
+    "Comparison Models for Type I SS",
+    "Model: y ~ a * b * c",
+    ""
+  ))
+
+  model <- lm(mpg ~ hp * disp, mtcars)
+  printed <- capture.output(generate_models(model, type = 1))
+  expect_identical(printed[[2]], "Model: mpg ~ hp * disp")
+
+  model <- lm(mpg ~ NULL, mtcars)
+  printed <- capture.output(generate_models(model, type = 1))
+  expect_identical(printed[2:4], c(
+    "Model: mpg ~ NULL",
+    "",
+    "No comparisons for empty model."
+  ))
+})
+
+test_that("model object has human readable printing", {
+  short_model <- y ~ a * b
+  printed <- capture.output(generate_models(short_model, type = 1))
+  expect_identical(printed[4:15], c(
+    "Full Model",
+    "  complex: y ~ a + b + a:b",
+    "   simple: y ~ NULL",
+    "a",
+    "  complex: y ~ a",
+    "   simple: y ~ NULL",
+    "b",
+    "  complex: y ~ a + b",
+    "   simple: y ~ a",
+    "a:b",
+    "  complex: y ~ a + b + a:b",
+    "   simple: y ~ a + b"
+  ))
+})
+
+test_that("type II models have spaces for removed terms", {
+  printed <- capture.output(generate_models(y ~ a * b, type = 2))
+  expect_identical(printed[7:15], c(
+    "a",
+    "  complex: y ~ a + b",
+    "   simple: y ~     b",
+    "b",
+    "  complex: y ~ a + b",
+    "   simple: y ~ a",
+    "a:b",
+    "  complex: y ~ a + b + a:b",
+    "   simple: y ~ a + b"
+  ))
+})
+
+test_that("type III models have spaces for removed terms", {
+  printed <- capture.output(suppressWarnings(generate_models(test_formula, type = 3)))
+  expect_identical(printed[7:27], c(
+    "a",
+    "  complex: y ~ a + b + c + a:b + a:c + b:c + a:b:c",
+    "   simple: y ~     b + c + a:b + a:c + b:c + a:b:c",
+    "b",
+    "  complex: y ~ a + b + c + a:b + a:c + b:c + a:b:c",
+    "   simple: y ~ a +     c + a:b + a:c + b:c + a:b:c",
+    "c",
+    "  complex: y ~ a + b + c + a:b + a:c + b:c + a:b:c",
+    "   simple: y ~ a + b +     a:b + a:c + b:c + a:b:c",
+    "a:b",
+    "  complex: y ~ a + b + c + a:b + a:c + b:c + a:b:c",
+    "   simple: y ~ a + b + c +       a:c + b:c + a:b:c",
+    "a:c",
+    "  complex: y ~ a + b + c + a:b + a:c + b:c + a:b:c",
+    "   simple: y ~ a + b + c + a:b +       b:c + a:b:c",
+    "b:c",
+    "  complex: y ~ a + b + c + a:b + a:c + b:c + a:b:c",
+    "   simple: y ~ a + b + c + a:b + a:c +       a:b:c",
+    "a:b:c",
+    "  complex: y ~ a + b + c + a:b + a:c + b:c + a:b:c",
+    "   simple: y ~ a + b + c + a:b + a:c + b:c"
+  ))
+})
+
+test_that("generate_models works with lm objects also", {
+  model <- lm(mpg ~ hp * disp, mtcars)
+  printed <- capture.output(suppressWarnings(generate_models(model, type = 3)))
+  expect_identical(printed[4:15], c(
+    "Full Model",
+    "  complex: mpg ~ hp + disp + hp:disp",
+    "   simple: mpg ~ NULL",
+    "hp",
+    "  complex: mpg ~ hp + disp + hp:disp",
+    "   simple: mpg ~      disp + hp:disp",
+    "disp",
+    "  complex: mpg ~ hp + disp + hp:disp",
+    "   simple: mpg ~ hp +        hp:disp",
+    "hp:disp",
+    "  complex: mpg ~ hp + disp + hp:disp",
+    "   simple: mpg ~ hp + disp"
+  ))
 })
